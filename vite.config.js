b902405f68d274cff6860a4ee7aef91a1dc223c3
Atish Patel <patelatish2001@gmail.com>
2024-08-19 11:42:27 +0530

    update with json server

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
    
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://json-9v1u.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
